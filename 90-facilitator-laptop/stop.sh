476f56712f20f548eca062f75920002622b0f166
James Harmison <jharmison@redhat.com>
2023-01-23 19:10:24 -0500

    Add removal of old networks

--- 90-facilitator-laptop/stop.sh
+++ 90-facilitator-laptop/stop.sh
@@ -6,8 +6,18 @@ cd "$SCRIPT_DIR" || fail Unable to cd into the script directory
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR/../common.sh"
 
+changed=false
 if [ "$(sudo nmcli c show "$LAB_WAN_NM_CONN" | awk '/^connection\.zone/{print $2}')" != "--" ]; then
     sudo nmcli c mod "$LAB_WAN_NM_CONN" connection.zone ""
+    changed=true
+fi
+if sudo nmcli c | grep -F rhte; then
+    sudo nmcli c down rhte
+    sudo nmcli c del rhte
+    changed=true
+fi
+if $changed; then
+    sudo systemctl restart NetworkManager
 fi
 for service in https dns dhcp; do
     sudo firewall-cmd --remove-service=$service --permanent --zone=internal
