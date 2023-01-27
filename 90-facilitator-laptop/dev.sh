3dd645c0b1bbb4fb3e6e0aab12b32a7a5d822efe
James Harmison <jharmison@redhat.com>
2023-01-27 16:21:19 -0500

    Let dev script at least scaffold real data

--- 90-facilitator-laptop/dev.sh
+++ 90-facilitator-laptop/dev.sh
@@ -6,7 +6,11 @@ cd "$SCRIPT_DIR" || fail Unable to cd into the script directory
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR/../common.sh"
 
-rm -rf "$PROJECT_DIR/90-facilitator-laptop/lab/content/"{_data/login.yml,_site,.jekyll-metadata,Gemfile.lock}
+rm -rf "$PROJECT_DIR/90-facilitator-laptop/lab/content/"{_data/login.yml,_data/passwords.yml,_site,.jekyll-metadata,Gemfile.lock}
+for cluster in $(seq "$METAL_CLUSTER_COUNT"); do
+    name="dev$cluster"
+    echo "$name: fakepassword$cluster" >> lab/content/_data/passwords.yml
+done
 < lab/content/_data/login.yml.tpl envsubst '$KUBEADMIN_PASS $LAB_USER_PASSWORD $CLUSTER_NAME $BASE_DOMAIN $INFRA_ENV' > lab/content/_data/login.yml
 
 podman build lab -t labguide:dev -f Containerfile.dev --build-arg BUILD_REVISION="$(git rev-parse HEAD)"
